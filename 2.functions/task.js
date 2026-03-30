a137f333eccc7cb24f7d062dfc4a1abaa9955fc5
alexandrabotvinova1406-del <alexandra.botvinova.1406@gmail.com>
2026-03-30 23:20:05 +0300

    second-task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,4 @@
 function getArrayParams(...arr) {
-
   if (arr.length === 0) {
     return { min: 0, max: 0, avg: 0 };
   }
@@ -14,19 +13,51 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
-
+  if (arr.length === 0) return 0;
+  
+  return arr.reduce((acc, curr) => acc + curr, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
+  if (arr.length === 0) return 0;
 
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+
+  return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  if (arr.length === 0) return 0;
+  
+  let sumEven = 0;
+  let sumOdd = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEven += arr[i];
+    } else {
+      sumOdd += arr[i];
+    }
+  }
+  
+  return sumEven - sumOdd;
 }
 
 function averageEvenElementsWorker(...arr) {
+  if (arr.length === 0) return 0;
 
+  let sumEven = 0;
+  let countEven = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEven += arr[i];
+      countEven++;
+    }
+  }
+  
+  return maxWorkerResult;
 }
 
 function makeWork (arrOfArr, func) {
