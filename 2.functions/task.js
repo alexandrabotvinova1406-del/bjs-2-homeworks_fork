1da54aac6d0e991800f6b024271e8e617cf2e674
alexandrabotvinova1406-del <alexandra.botvinova.1406@gmail.com>
2026-03-30 23:09:36 +0300

    first-task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,15 @@
 function getArrayParams(...arr) {
 
+  if (arr.length === 0) {
+    return { min: 0, max: 0, avg: 0 };
+  }
+
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+
+  const sum = arr.reduce((acc, curr) => acc + curr, 0);
+  const avg = Number((sum / arr.length).toFixed(2));
+
   return { min: min, max: max, avg: avg };
 }
 
